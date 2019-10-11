453f80b404c56a230eb9f3373d812c1d3e5cbe89
Robert Günzler <r@gnzler.io>
2019-10-11 14:31:11 +0200

    Implement more rsync capabilities
    Fixes errors related to sudo

--- vagrant-plugin/lib/vagrant-balenaos/cap/rsync.rb
+++ vagrant-plugin/lib/vagrant-balenaos/cap/rsync.rb
@@ -1,9 +1,34 @@
+require "shellwords"
+
 module VagrantPlugins
   module GuestBalenaOS
     module Cap
       class RSync
         def self.rsync_installed(machine)
-          machine.communicate.test('test -f /usr/bin/rsync')
+          machine.communicate.test('which rsync')
+        end
+
+        def self.rsync_command(machine)
+          "#{machine.config.balenaos.suexec_cmd} rsync"
+        end
+
+        def self.rsync_pre(machine, opts)
+          machine.communicate.tap do |comm|
+            comm.execute("#{machine.config.balenaos.suexec_cmd} mkdir -p '#{opts[:guestpath]}'")
+          end
+        end
+
+        def self.rsync_post(machine, opts)
+          if opts.key?(:chown) && !opts[:chown]
+            return
+          end
+          suexec_cmd = machine.config.balenaos.suexec_cmd
+          machine.communicate.execute("#{suexec_cmd} #{self.build_rsync_chown(opts)}")
+        end
+
+        def self.build_rsync_chown(opts)
+          guest_path = Shellwords.escape(opts[:guestpath])
+          "chown #{opts[:owner]}:#{opts[:group]} -R #{guest_path}"
         end
       end
     end
--- vagrant-plugin/lib/vagrant-balenaos/plugin.rb
+++ vagrant-plugin/lib/vagrant-balenaos/plugin.rb
@@ -43,6 +43,21 @@ module VagrantPlugins
         require_relative 'cap/rsync'
         Cap::RSync
       end
+
+      guest_capability('balenaos', 'rsync_command') do
+        require_relative 'cap/rsync'
+        Cap::RSync
+      end
+
+      guest_capability('balenaos', 'rsync_pre') do
+        require_relative 'cap/rsync'
+        Cap::RSync
+      end
+
+      guest_capability('balenaos', 'rsync_post') do
+        require_relative 'cap/rsync'
+        Cap::RSync
+      end
     end
   end
 end
